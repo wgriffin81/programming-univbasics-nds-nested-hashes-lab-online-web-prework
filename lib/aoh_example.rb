fb3c7cfe210e17fb586e2edcf5f856c33e707b63
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-22 17:29:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,4 +31,6 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aah[row][key] = new_value
+  return aah
 end
